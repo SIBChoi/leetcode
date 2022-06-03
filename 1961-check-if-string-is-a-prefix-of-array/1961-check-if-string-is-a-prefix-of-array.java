8e1c1274d4921add73c98d14b7429e62cae07b6a
Choi- Chanho <74345026+SIBChoi@users.noreply.github.com>
2022-06-03 17:00:17 +0900

    Time: 2 ms (60.15%), Space: 42.6 MB (69.83%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
@@ -1,16 +1,29 @@
 class Solution {
     public boolean isPrefixString(String s, String[] words) {
-        String tmp = "";
+        StringBuilder sb = new StringBuilder();
         
         for (String str: words) {
-            tmp += str;
-            if(tmp.equals(s)) {
+            sb.append(str);
+            if (s.equals(sb.toString())) {
                 return true;
-            } else if(tmp.length() > s.length()) {
+            } else if (s.indexOf(sb.toString()) == -1) {
                 return false;
             }
         }
-     
+        
         return false;
+        
+//         String tmp = "";
+        
+//         for (String str: words) {
+//             tmp += str;
+//             if(tmp.equals(s)) {
+//                 return true;
+//             } else if(tmp.length() > s.length()) {
+//                 return false;
+//             }
+//         }
+     
+//         return false;
     }
 }
