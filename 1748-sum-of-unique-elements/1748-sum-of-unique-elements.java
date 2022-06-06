cc915fee685cbb93c98b8b6fad14ccc9f920461d
Choi- Chanho <74345026+SIBChoi@users.noreply.github.com>
2022-06-06 17:57:23 +0900

    Time: 2 ms (52.15%), Space: 42 MB (29.04%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -3,20 +3,27 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap();
         int output = 0;
         
+//         for (int i = 0; i < nums.length; i++) {
+//             if(map.containsKey(nums[i])) {
+//                 map.put(nums[i], map.get(nums[i]) + 1);
+//             } else {
+//                 map.put(nums[i], 1);
+//             }
+//         }
+        
+//         for (int key: map.keySet().toArray(new Integer[map.size()])) {
+//             if(map.get(key) == 1) {
+//                 output += key;
+//             }
+//         }
         for (int i = 0; i < nums.length; i++) {
-            if(map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-            } else {
-                map.put(nums[i], 1);
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if(map.get(nums[i]) == 1) {
+                output += nums[i];
+            } else if(map.get(nums[i]) == 2) {
+                output -= nums[i];
             }
         }
-        
-        for (int key: map.keySet().toArray(new Integer[map.size()])) {
-            if(map.get(key) == 1) {
-                output += key;
-            }
-        }
-        
         return output;
     }
 }
