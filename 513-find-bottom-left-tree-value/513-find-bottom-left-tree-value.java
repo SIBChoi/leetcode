ebae430ceb0e2bc3313814ae9a4df26ca94b00ce
Choi- Chanho <74345026+SIBChoi@users.noreply.github.com>
2022-06-07 20:45:54 +0900

    Time: 2 ms (47.84%), Space: 43 MB (70.07%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.java
@@ -17,12 +17,13 @@ class Solution {
     public int findBottomLeftValue(TreeNode root) {
        Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
+        
         while(!queue.isEmpty()) {
             root = queue.poll();
-            if(root.right != null) {
+            if (root.right != null) {
                 queue.add(root.right);
             }
-            if(root.left != null) {
+            if (root.left != null) {
                 queue.add(root.left);
             }
         }
