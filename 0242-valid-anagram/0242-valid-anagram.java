24017011578cae05ce62cf7f53776b16c8b9007c
Choi- Chanho <74345026+SIBChoi@users.noreply.github.com>
2022-10-24 10:32:25 +0900

    Time: 95 ms (5.03%), Space: 50.3 MB (5.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,30 +4,10 @@ class Solution {
             return false;
         }
         
-        boolean anagram = true;
-        
-        Map<String, Integer> map = new HashMap<>();
-        
-        for (String s1 : s.split("")) {
-            map.put(s1, map.getOrDefault(s1, 0) + 1);
-        }
-        
-        for (String t1 : t.split("")) {
-            if (!map.containsKey(t1)) {
-                anagram = false;
-                break;
-            } else {
-                map.put(t1, map.getOrDefault(t1, 0) - 1);
-                
-                if (map.get(t1) < 0) {
-                    anagram = false;
-                    break;
-                }
-            }
-        }
+        String s1 = Arrays.stream(s.split("")).sorted().collect(Collectors.joining());
+        String t1 = Arrays.stream(t.split("")).sorted().collect(Collectors.joining());
         
         
-        
-        return anagram;
+        return s1.equals(t1);
     }
 }
