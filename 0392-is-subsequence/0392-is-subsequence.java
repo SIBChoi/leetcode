a7f51165f9ffa4a81c52c42a490dfeec6b6888b8
Choi- Chanho <74345026+SIBChoi@users.noreply.github.com>
2022-10-25 10:05:25 +0900

    Time: 2 ms (67.03%), Space: 41.6 MB (78.92%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,22 +1,33 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if ("".equals(s)) {
-            return true;
-        }
+        // if ("".equals(s)) {
+        //     return true;
+        // }
         
-        boolean answer = false;
+//         boolean answer = false;
         
-        int idx2 = 0;
-        for (int i = 0; i < t.length(); i++) {
-            if (s.charAt(idx2) == t.charAt(i)) {
-                idx2++;
-                if (idx2 == s.length()) {
-                    answer = true;
-                    break;
-                }
+//         int idx2 = 0;
+//         for (int i = 0; i < t.length(); i++) {
+        //     if (s.charAt(idx2) == t.charAt(i)) {
+        //         idx2++;
+        //         if (idx2 == s.length()) {
+        //             answer = true;
+        //             break;
+        //         }
+        //     }
+        // }
+        
+        // return answer;
+        
+         if (s.length() == 0) return true;
+        int indexS = 0, indexT = 0;
+        while (indexT < t.length()) {
+            if (t.charAt(indexT) == s.charAt(indexS)) {
+                indexS++;
+                if (indexS == s.length()) return true;
             }
+            indexT++;
         }
-        
-        return answer;
+        return false;
     }
 }
